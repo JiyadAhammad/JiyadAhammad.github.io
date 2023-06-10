224b5a6fc7ab2b5318058bf325b50a803e8438d8
Jiyad Ahammad <105364728+JiyadAhammad@users.noreply.github.com>
2023-06-10 18:24:22 +0400

    Add files via upload
    web build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,15 +2,65 @@
 const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
-const RESOURCES = {
-  "assets/asset/jpg/sample.jpg": "29320f85377b3aa644e03c004a21e0c3",
+
+const RESOURCES = {"assets/asset/lottie/chatbot.json": "955f9448fe03e5931e65892fdc5e40b1",
+"assets/asset/lottie/Filter.json": "e545f098fad97850ada339ae90fdd0ff",
+"assets/asset/lottie/Share.json": "c58199d3ad6290b5b4ad3bd6c15e6020",
+"assets/asset/lottie/soon.json": "cbbfd624f71d5f188a6dd9b71530fe2c",
+"assets/asset/lottie/sorting.json": "662a0847bf9b3d5d5a96883ca2ec1288",
+"assets/asset/lottie/store-review.json": "ef90d321515aa03fd81ddf5b0a803de0",
+"assets/asset/lottie/Store.json": "a7c6b460b7e99c2df4790ccd0a578f95",
+"assets/asset/lottie/Watch.json": "a649484ae38b4f723bdf4cd05517fb44",
+"assets/asset/lottie/Way.json": "c98c4932be0cdae81e60e60a01b9fbbb",
+"assets/asset/my-store/1.png": "5c59d47d410d243762ce1d36f9cb58f4",
+"assets/asset/my-store/2.png": "bbbe935af624022afab80bb1ac4f6cc0",
+"assets/asset/my-store/3.png": "3f446436fb1fe421a21507618957a8a8",
+"assets/asset/my-store/4.png": "b3d8411a813718f09cf10672460168ef",
+"assets/asset/my-store/address.png": "fc65a0112bf5369d1809507862edfee6",
+"assets/asset/my-store/banner.webp": "b7648352e291ac5c8f2c0b5fc1be23b7",
+"assets/asset/my-store/bannerr.webp": "711c263fbbc62f23adecebfb363a428e",
+"assets/asset/my-store/book.png": "b0abff77c09f25d9299092fff937b18e",
+"assets/asset/my-store/cafe.jpg": "02c3932924fc51980b50f8197e6056fa",
+"assets/asset/my-store/calender.png": "476173c5f6552edbc4a623c30f39df29",
+"assets/asset/my-store/call.png": "c25e7406291415fa47b497b8f0ac9e8f",
+"assets/asset/my-store/car.jpg": "e37231fccbb8045bfb94afffd733f61e",
+"assets/asset/my-store/cart.png": "5642067d4cce236837278cde6fa0529f",
+"assets/asset/my-store/cartimg.png": "a66f77359e23bfca01bd1c5df9fc2866",
+"assets/asset/my-store/chat.png": "cc9eb669a9b5b99aeecab9b49bcea56b",
+"assets/asset/my-store/closed-store-info.png": "eb583aeae8ece8c781be686455f76b3b",
+"assets/asset/my-store/clothes.png": "f4c533731d2ad6e83124c3a62c95d3ef",
+"assets/asset/my-store/empty-cart.png": "d2f9ffe26adee1fa3aceefbad426232d",
+"assets/asset/my-store/fertilizer.png": "82aa82688f85a54b56982f22572488fd",
+"assets/asset/my-store/food.png": "755c566fc0fb4279b4b035fa40290a88",
+"assets/asset/my-store/grid.png": "88fb1b0ed2981fcc1e71712da20f7ce7",
+"assets/asset/my-store/home.png": "7d11a19c2bd6a20e577d8a2a9c3c5553",
+"assets/asset/my-store/list.png": "fac715cc2965622c984625d57db02581",
+"assets/asset/my-store/locate.png": "bc51aff4b51db042e3b591050a14544b",
+"assets/asset/my-store/locate_unselect.png": "9a0a8b9fc72cb0e6f60a93b350d00cca",
+"assets/asset/my-store/logo.png": "f236773723347ffd3403bf5c5d0dfd6b",
+"assets/asset/my-store/navoffer-selected.png": "ad8213b5d1038d08cca2decfa5547657",
+"assets/asset/my-store/navoffer-unselected.png": "f2858664535ef47978e3b6fa9f4718be",
+"assets/asset/my-store/newLogo.png": "bc2d3e696e60714eba6405b1e42a9a5e",
+"assets/asset/my-store/no-promotions.png": "1a56a783fc323f5026cfff165ca36e61",
+"assets/asset/my-store/percent.png": "b0f0451045e81bbe6f23e3f77baef68d",
+"assets/asset/my-store/percentage.png": "22593dc2d15e8fa29646136ab2dcf0e2",
+"assets/asset/my-store/PLUS-removebg-preview.png": "69322dde348de413f65eecf50b0f1b79",
+"assets/asset/my-store/popcorn.png": "e13306dbb86f94dd336e55ce8744bc0d",
+"assets/asset/my-store/postion.png": "956a750bb6a90240ac64ec3402c72ad3",
+"assets/asset/my-store/pro.jpg": "ccffe047a3836b033deeee8f130aaad7",
+"assets/asset/my-store/sale.png": "3a270c57aa63cd4141934f0b6e1463f9",
+"assets/asset/my-store/soap.png": "39a6f8de90cf07444c87b153c55b86a5",
+"assets/asset/my-store/store.png": "620b098cd3b7c9c507e628b3e2b7d4ec",
+"assets/asset/my-store/unselected_cart.png": "2aecb19fc2d31fbffeb0c3e90866701d",
+"assets/asset/my-store/unselected_location.png": "e66018773d906692223b719a4cbf4240",
+"assets/asset/my-store/unselected_location.png.png": "9a0a8b9fc72cb0e6f60a93b350d00cca",
+"assets/asset/my-store/unselected_percent.png": "5ade583ed7069b6813579a6c0f3a279b",
 "assets/asset/png/1.png": "5c59d47d410d243762ce1d36f9cb58f4",
 "assets/asset/png/2.png": "bbbe935af624022afab80bb1ac4f6cc0",
 "assets/asset/png/3.png": "3f446436fb1fe421a21507618957a8a8",
 "assets/asset/png/4.png": "b3d8411a813718f09cf10672460168ef",
 "assets/asset/png/banner.webp": "b7648352e291ac5c8f2c0b5fc1be23b7",
 "assets/asset/png/bannerr.webp": "711c263fbbc62f23adecebfb363a428e",
-"assets/asset/png/basket.png": "60e5e8467403fc87d8bc88a7288861b0",
 "assets/asset/png/book.png": "b0abff77c09f25d9299092fff937b18e",
 "assets/asset/png/cafe.jpg": "02c3932924fc51980b50f8197e6056fa",
 "assets/asset/png/calender.png": "476173c5f6552edbc4a623c30f39df29",
@@ -20,52 +70,51 @@ const RESOURCES = {
 "assets/asset/png/clothes.png": "f4c533731d2ad6e83124c3a62c95d3ef",
 "assets/asset/png/fertilizer.png": "82aa82688f85a54b56982f22572488fd",
 "assets/asset/png/food.png": "755c566fc0fb4279b4b035fa40290a88",
-"assets/asset/png/home.png": "e8ad3c6f9e69c1091bcb4f5e36071d0c",
+"assets/asset/png/home.png": "7d11a19c2bd6a20e577d8a2a9c3c5553",
 "assets/asset/png/locate.png": "db96f981ac0924882081826bd3349445",
 "assets/asset/png/logo.png": "f236773723347ffd3403bf5c5d0dfd6b",
-"assets/asset/png/oonzoo.png": "f08cf0ed15906ca52913bf051bf8323c",
+"assets/asset/png/newLogo.png": "bc2d3e696e60714eba6405b1e42a9a5e",
+"assets/asset/png/no-store-img.jpg": "6bcaf4f4fa927793d622144c0f89552e",
 "assets/asset/png/percent.png": "b0f0451045e81bbe6f23e3f77baef68d",
 "assets/asset/png/PLUS-removebg-preview.png": "69322dde348de413f65eecf50b0f1b79",
 "assets/asset/png/popcorn.png": "e13306dbb86f94dd336e55ce8744bc0d",
+"assets/asset/png/postion.png": "956a750bb6a90240ac64ec3402c72ad3",
 "assets/asset/png/pro.jpg": "ccffe047a3836b033deeee8f130aaad7",
 "assets/asset/png/sale.png": "3a270c57aa63cd4141934f0b6e1463f9",
-"assets/asset/png/share.png": "8d90dbe60bf6dab67d3c25b3d1418fd7",
-"assets/asset/png/showroom.jpg": "feceacb1bd8c237fd4b5b8fa833a01a2",
-"assets/asset/png/showroom2.jpeg": "4c8d86f29359123387d0814682f46390",
-"assets/asset/png/showroom3.jpeg": "65038246f691ecf71dc90d8ad633fdcb",
 "assets/asset/png/soap.png": "39a6f8de90cf07444c87b153c55b86a5",
 "assets/asset/png/store.png": "620b098cd3b7c9c507e628b3e2b7d4ec",
-"assets/asset/png/user.png": "cac2522998ee7dff23e9502b749ea9fb",
-"assets/AssetManifest.json": "17644996f819281c574a57d794dd207c",
+"assets/AssetManifest.bin": "0749e9c74e33cb8332b121662aae9eb0",
+"assets/AssetManifest.json": "b4b2ce59e83dd9cc655092a67affe29d",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"assets/NOTICES": "df048b04357f45c6bb4884e7648dc86e",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/shaders/ink_sparkle.frag": "5000f7ca8eb663d2b67d7f003984cafc",
-"canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
-"canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
-"canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
-"canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
-"favicon.png": "90a94662e8bf238f5003a8be2efec2cd",
-"flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
-"icons/Icon-192.png": "4afd99f5fd180b445c384981a66e958e",
-"icons/Icon-512.png": "b20780f071aa0e3fbde5016621a9fbc8",
-"icons/Icon-maskable-192.png": "4afd99f5fd180b445c384981a66e958e",
-"icons/Icon-maskable-512.png": "b20780f071aa0e3fbde5016621a9fbc8",
-"index.html": "806b3fcc611d760a133fcd1986db8b7e",
-"/": "806b3fcc611d760a133fcd1986db8b7e",
-"main.dart.js": "40dd7ee55179c32b34650da837b502b8",
-"manifest.json": "f66e44e81aff40b59fd1bb26b04c1b42",
-"version.json": "9094aacdae789dccd67fa32109ff1a18"
-};
-
+"assets/fonts/MaterialIcons-Regular.otf": "f9c2152b7e88e68cab12a79d13fd7690",
+"assets/NOTICES": "5dfdc5231d93522d0edf82ae7e5b98db",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "57d849d738900cfd590e9adc7e208250",
+"assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
+"canvaskit/canvaskit.js": "76f7d822f42397160c5dfc69cbc9b2de",
+"canvaskit/canvaskit.wasm": "f48eaf57cada79163ec6dec7929486ea",
+"canvaskit/chromium/canvaskit.js": "8c8392ce4a4364cbb240aa09b5652e05",
+"canvaskit/chromium/canvaskit.wasm": "fc18c3010856029414b70cae1afc5cd9",
+"canvaskit/skwasm.js": "1df4d741f441fa1a4d10530ced463ef8",
+"canvaskit/skwasm.wasm": "6711032e17bf49924b2b001cef0d3ea3",
+"canvaskit/skwasm.worker.js": "19659053a277272607529ef87acf9d8a",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"flutter.js": "6b515e434cea20006b3ef1726d2c8894",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"index.html": "045f24b08ee180139731bc6bcfc53995",
+"/": "045f24b08ee180139731bc6bcfc53995",
+"main.dart.js": "4c179396ee6c1145c77901305f590a12",
+"manifest.json": "c5af9fb83207de8f87e281089d3fa8bc",
+"version.json": "5fdf2f6e7cf33f284e3fc3b95d073128"};
 // The application shell files that are downloaded before a service worker can
 // start.
-const CORE = [
-  "main.dart.js",
+const CORE = ["main.dart.js",
 "index.html",
 "assets/AssetManifest.json",
 "assets/FontManifest.json"];
+
 // During install, the TEMP cache is populated with the application shell files.
 self.addEventListener("install", (event) => {
   self.skipWaiting();
@@ -76,7 +125,6 @@ self.addEventListener("install", (event) => {
     })
   );
 });
-
 // During activate, the cache is populated with the temp files downloaded in
 // install. If this service worker is upgrading from one with a saved
 // MANIFEST, then use this to retain unchanged resource files.
@@ -98,6 +146,8 @@ self.addEventListener("activate", function(event) {
         await caches.delete(TEMP);
         // Save the manifest to make future upgrades efficient.
         await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
         return;
       }
       var oldManifest = await manifest.json();
@@ -123,6 +173,8 @@ self.addEventListener("activate", function(event) {
       await caches.delete(TEMP);
       // Save the manifest to make future upgrades efficient.
       await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
       return;
     } catch (err) {
       // On an unhandled exception the state of the cache cannot be guaranteed.
@@ -133,7 +185,6 @@ self.addEventListener("activate", function(event) {
     }
   }());
 });
-
 // The fetch handler redirects requests for RESOURCE files to the service
 // worker cache.
 self.addEventListener("fetch", (event) => {
@@ -173,7 +224,6 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
-
 self.addEventListener('message', (event) => {
   // SkipWaiting can be used to immediately activate a waiting service worker.
   // This will also require a page refresh triggered by the main worker.
@@ -186,7 +236,6 @@ self.addEventListener('message', (event) => {
     return;
   }
 });
-
 // Download offline will check the RESOURCES for all files not in the cache
 // and populate them.
 async function downloadOffline() {
@@ -207,7 +256,6 @@ async function downloadOffline() {
   }
   return contentCache.addAll(resources);
 }
-
 // Attempt to download the resource online before falling back to
 // the offline cache.
 function onlineFirst(event) {
